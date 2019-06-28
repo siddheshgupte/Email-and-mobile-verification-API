f007536b92f32d7289e3b4bf7ecaa7146da44510
Siddhesh Gupte <siddhesh.gupte2468@gmail.com>
2019-06-28 13:07:14 -0500

    Delete credentials.py

--- ev/mobileverification/credentials.py
+++
@@ -1,6 +0,0 @@
-# Enter your twilio number and your cell number to check
-
-account_sid="ACd237fc2838b4cc8114472136ce82cbab"
-auth_token="2af639b6d84697bba54d9716810305c0"
-my_cell='+919619157422'
-my_twilio='+12163735937'
